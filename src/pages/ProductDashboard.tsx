c191a8c3906772b56581b4230f8186b132532209
Dhanushka Gimhan <dhanushkagimhan@gmail.com>
2024-04-12 14:09:37 +0530

    init the project

---
+++ src/pages/ProductDashboard.tsx
@@ -0,0 +1,7 @@
+export default function ProductDashboard() {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
---
+++ src/pages/index.ts
@@ -0,0 +1,3 @@
+import ProductDashboard from "./ProductDashboard";
+
+export { ProductDashboard };
