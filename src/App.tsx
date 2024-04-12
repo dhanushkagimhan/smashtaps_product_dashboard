67dfee213d2eb2cf1089458fb001d5d1f7304325
Dhanushka Gimhan <dhanushkagimhan@gmail.com>
2024-04-12 14:18:26 +0530

    resolve the error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { ProductDashboard } from "./pages";
 
 function App() {
--- src/pages/ProductDashboard.tsx
+++ src/pages/ProductDashboard.tsx
@@ -1,7 +1,7 @@
 export default function ProductDashboard() {
   return (
     <div>
-      <div></div>
+      <div>s</div>
     </div>
   );
 }
